75cc90e0e91fb61c44c7cfbe08e379ce5881452d
Richard Hernandez <hern784@usc.edu>
2020-05-08 00:33:09 -0700

    init commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define Paths fr Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('views', viewsPath)
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Richard Hernandez'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'Richard Hernandez'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpPage: 'This is the help page',
+        title: 'Help Page',
+        name: 'Richard Hernandez'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an Address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Richard Hernandez',
+        errorMessage: 'Help article Not found'
+    })
+})
+
+// 404 Error
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Richard Hernandez',
+        errorMessage: 'Page Not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=9f01eed927974e7614029c36b905f353&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=f'
+    request({url, json: true}, (error, { body }) => {
+    if (error) {
+        callback('unable to connect to location services', undefined)
+    }
+    else if (body.error) {
+        callback('unable to find location try another search', undefined)
+    }
+    else {
+        callback(undefined, body.current.weather_descriptions[0] + ". it is currently " + body.current.temperature + " degrees out, but it feels like " + body.current.feelslike + " degrees.")
+    }
+})
+
+}
+
+module.exports = forecast
