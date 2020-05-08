8908a68d9dd09f71de6a136d3d85431f8f9b1677
Richard Hernandez <hern784@usc.edu>
2020-05-08 01:09:58 -0700

    added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,11 @@ const forecast = (latitude, longitude, callback) => {
         callback('unable to find location try another search', undefined)
     }
     else {
-        callback(undefined, body.current.weather_descriptions[0] + ". it is currently " + body.current.temperature + " degrees out, but it feels like " + body.current.feelslike + " degrees.")
+        callback(
+            undefined,
+            body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature +
+            " degrees out, but it feels like " + body.current.feelslike + " degrees. The humidity is " + body.current.humidity + "%."
+        )
     }
 })
 
